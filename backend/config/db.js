cc89bd52506ab44dd963c3316886537c40d2c221
Hemanth T G <hemanthjayanesh@gmail.com>
2026-08-15 18:38:40 +0530

    Move database credentials to environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://HemanthTG:31Aug2005@cluster0.j4vu0oq.mongodb.net/tomato').then(()=>console.log("Database is connected."))
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("Database is connected."))
 }
-
-
-// mongodb+srv://HemanthTG:31Aug2005@cluster0.j4vu0oq.mongodb.net/tomato
